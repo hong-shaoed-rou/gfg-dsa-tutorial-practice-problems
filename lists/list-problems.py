f9aa3e3b26c367bf14982e1a488bef5765ffbcb4
hong-shaoed-rou <armanilearns@gmail.com>
2026-05-25 16:13:22 -0400

    added list practice problems

---
+++ lists/list-problems.py
@@ -0,0 +1,56 @@
+from typing import List
+
+# Given an array arr[], return a list elements of arr 
+# in alternate order (starting from index 0).
+#
+# Example:
+# Input: arr[] = [1, 2, 3, 4, 5]
+# Output: 1 3 5
+
+def alts(arr: List[int]) -> List[int]:
+
+    pass
+
+# Given an array, arr[] of n integers, and an integer element x, find whether
+# element x is present in the array. Return the index of the first occurrence
+# of x in the array, or -1 if it doesn't exist.
+#
+# Example:
+# Input: arr[] = [1, 2, 3, 4], x = 3
+# Output: 2
+
+def find(arr: List[int]) -> int:
+
+    pass
+
+# Given an array arr[]. The task is to find the largest element and return it.
+#
+# Example:
+# Input: arr[] = [1, 8, 7, 56, 90]
+# Output: 90
+
+def largest(arr: List[int]) -> int:
+
+    pass
+
+
+# Given an array of integers arr[]. Reverse the given array in place.
+#
+# Example:
+# Input: arr = [1, 4, 3, 2, 6, 5]
+# Output: [5, 6, 2, 3, 4, 1]
+
+def reverse(arr: List[int]) -> None:
+
+    pass
+
+# Given an array arr[], check whether it is sorted in non-decreasing order.
+# Return true if it is sorted otherwise false.
+#
+# Example:
+# Input: arr[] = [10, 20, 30, 40, 50]
+# Output: true
+
+def sorted(arr: List[int]) -> bool:
+
+    pass
